90f44d76ae84a6c82ca618c488a1ca1de39929c4
Lee <51656281@qq.com>
2017-04-24 17:07:38 +0800

    no message

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
     public function index()
     {
         $user = Power::user();                  //根据用户信息抓取视图层需要的数据  （菜单，头像，姓名）
-        $user['active'] = '';
+        $user['active'] = 'home';
         $user['nav_active'] = '';
 
         return view('admin.index',['user'=>$user]);       //active:菜单选中状态   nav-active:跟菜单打开状态
