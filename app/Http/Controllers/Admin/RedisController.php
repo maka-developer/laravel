29e40b7f297bbbf08d5e20c2adfda8c2b41e1c41
cent <mail.loodp@gmail.com>
2017-04-24 16:21:33 +0800

    cent

--- app/Http/Controllers/Admin/RedisController.php
+++ app/Http/Controllers/Admin/RedisController.php
@@ -14,9 +14,18 @@ class RedisController extends Controller
         $user = Power::user();                  //根据用户信息抓取视图层需要的数据  （菜单，头像，姓名）
         $user['active'] = 'redis';
         $user['nav_active'] = '';
-
+		
+      	$arr = array();
         $keys = Redis::keys('*');
-        var_dump($keys);
+      	dd($keys);
+      	exit();
+      	if(!empty($keys){
+          foreach($keys as $item){
+            $arr[$item] = Redis::type($item);
+          }
+        }
+        dd($arr);
+      	exit();
 
         return view('admin.redis',['user'=>$user]);       //active:菜单选中状态   nav-active:跟菜单打开状态
     }
