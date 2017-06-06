50517b433dcaa56ff9129e06e402d7b34216b9c8
PeterPan <51656281@qq.com>
2017-06-06 22:02:38 +0800

    no message

--- app/Http/Controllers/Api/GitController.php
+++ app/Http/Controllers/Api/GitController.php
@@ -9,7 +9,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
 
 class GitController extends Controller
 {
@@ -18,36 +17,14 @@ class GitController extends Controller
     public function getGit(Request $request)
     {
         header("Content-type: text/html; charset=utf-8");
-        $resArr['request'] = $request;
-        $resArr['server'] = $_SERVER;
+//        $resArr['request'] = $request;
+//        $resArr['server'] = $_SERVER;
         $resArr['x-github-delivery'] = $request->header('x-github-delivery');
-        $resArr['sh1'] = $this->getSignature($this->key, $resArr['x-github-delivery']);
-        dd($resArr);
-    }
+        list($resArr['algo'], $resArr['hash']) = explode('=', $resArr['x-github-delivery'], 2);
 
-    function getSignature($str, $key) {
-        if (function_exists('hash_hmac')) {
-            $signature = base64_encode(hash_hmac("sha1", $str, $key, true));
-        } else {
-            $blocksize = 64;
-            $hashfunc = 'sha1';
-            if (strlen($key) > $blocksize) {
-                $key = pack('H*', $hashfunc($key));
-            }
-            $key = str_pad($key, $blocksize, chr(0x00));
-            $ipad = str_repeat(chr(0x36), $blocksize);
-            $opad = str_repeat(chr(0x5c), $blocksize);
-            $hmac = pack(
-                'H*', $hashfunc(
-                    ($key ^ $opad) . pack(
-                        'H*', $hashfunc(
-                            ($key ^ $ipad) . $str
-                        )
-                    )
-                )
-            );
-            $signature = base64_encode($hmac);
-        }
-        return $signature;
+        // 获取body内容
+        $resArr['payload'] = file_get_contents('php://input');
+        $resArr['payloadHash '] = hash_hmac($resArr['algo'], $resArr['payload'], $this->key);
+        dd($resArr);
     }
 }
