440a74c52f4b4d6ecbf9bd1bfbb4363b7297c774
Lee <51656281@qq.com>
2017-07-03 15:21:03 +0800

    no message

--- app/Http/Controllers/Admin/WxController.php
+++ app/Http/Controllers/Admin/WxController.php
@@ -8,13 +8,18 @@
 namespace app\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Libs\Curl;
 
 class WxController extends Controller
 {
 
     public function index()
     {
-        echo 1;
-        $url = "https://wx.loodp.com/wxbg?token=";
+        $rArr['token'] = md5(time() . rand(100000,999999));
+        $rArr['time'] = time();
+        $url = "https://wx.loodp.com/wxbg?token=".$rArr['token'];
+        $curl = new Curl($url);
+        $res = $curl->request([],'GET','',1,'all');
+        dd($res);
     }
 }
